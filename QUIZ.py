e111f886e693da87e62d797ce4bb2bafa4389cdc
CHOIJISEOK <102344269+CHOIJISEOK0@users.noreply.github.com>
2022-04-15 15:07:20 +0900

    +  FIND VERTICAL DIRECTION

--- QUIZ.py
+++ QUIZ.py
@@ -1,11 +1,55 @@
-#READ FILE
-with open("QUIZ.txt", "r") as f:
-         data = f.read()
+# READ FILE
+with open("code.txt", "r") as f:
+    data = f.read()
 
-#CUT SENTENCE
+# CUT SENTENCES
 a = data.split("\n")
 
+result = []
+
+word = "improve"
 for y in range(len(a)):
-        for x in range(len(a[0])):
-                if a[y][x] == "a":
-                           print(x,y)
+    for x in range(len(a[0])):
+        # a 가 있는 위치 찾기
+        if a[y][x] == word[0]:
+            # RIGHT
+            check = True
+            if x + len(word) - 1 < len(a[0]):
+                for b in range(len(word)):
+                    if a[y][x + b] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+
+            # LEFT
+            check = True
+            if x - len(word) + 1 >= 0:
+                for b in range(len(word)):
+                    if a[y][x - b] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+
+            # DOWM
+            check = True
+            if y + len(word) - 1 < len(a):
+                for b in range(len(word)):
+                    if a[y + b][x] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+
+            # UP
+            check = True
+            if y - len(word) + 1 >= 0:
+                for b in range(len(word)):
+                    if a[y - b][x] != word[b]:
+                        check = False
+                        break
+                if check:
+                    result.append([x, y])
+
+print(result)
